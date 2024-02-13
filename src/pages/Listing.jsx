48cd48dc4b287a4a4eadb845a26ed241c8e8569b
Yug063 <tyagiyug2018@gmail.com>
2024-02-13 18:50:32 +0530

    changes

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -32,7 +32,9 @@ export default function Listing() {
     const fetchListing = async () => {
       try {
         setLoading(true);
-        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const res = await fetch(
+          `https://key2homes-backend.onrender.com/api/listing/get/${params.listingId}`
+        );
         const data = await res.json();
         if (data.success === false) {
           setError(true);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "custom-red": "#521945",
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/line-clamp"),
-    // ...
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-};
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

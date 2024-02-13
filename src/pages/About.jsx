e6254a27f2af303adc9655c9b7fc3e3c254d05e1
Yug063 <tyagiyug2018@gmail.com>
2024-02-13 17:51:49 +0530

    frontend

---
+++ src/pages/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <div className="bg-blue-100  rounded-lg shadow-lg p-6">
+        <h1 className="text-3xl font-bold mb-4 text-custom-red">
+          About Key<span className="text-red-800">2</span>Homes
+        </h1>
+        <p className="mb-4 text-slate-700 border border-gray-300 rounded-lg shadow-md p-4">
+          Key2Homes is your premier real estate platform, specializing in helping
+          clients buy, sell, and rent properties in the most desirable
+          neighborhoods. Our dedicated team of experienced agents is committed to
+          delivering exceptional service, ensuring a seamless buying and selling
+          process.
+        </p>
+        <p className="mb-4 text-slate-700 border border-gray-300 rounded-lg shadow-md p-4">
+          Our mission at Key2Homes is to empower our clients to achieve their real
+          estate goals through expert advice, personalized service, and an
+          in-depth understanding of the local market. Whether you're looking to
+          buy, sell, or rent a property, we're here to support you every step of
+          the way.
+        </p>
+        <p className="mb-4 text-slate-700 border border-gray-300 rounded-lg shadow-md p-4">
+          Our team of agents brings a wealth of experience and knowledge to the
+          real estate industry, and we're unwavering in our commitment to
+          providing the highest level of service to our clients. We believe that
+          the process of buying or selling a property should be an exciting and
+          rewarding experience, and Key2Homes is dedicated to making that a
+          reality for each and every one of our clients.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function () {
+  return (
+    <footer className="bottom-0 w-full bg-blue-500 bg-opacity-60 shadow-lg text-white py-6">
+      <div className="container mx-auto text-center">
+        <p className="text-md">
+          &copy; {new Date().getFullYear()} Key2Homes. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+// bg-indigo-600 bg-opacity-50

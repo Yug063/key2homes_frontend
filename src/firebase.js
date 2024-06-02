a470d6dbeef71a8d415798ecd5673dddc80f3cea
Yug063 <tyagiyug2018@gmail.com>
2024-06-02 23:17:29 +0530

    changes in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,7 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: 'mern-estate.firebaseapp.com',
-//   projectId: 'mern-estate',
-//   storageBucket: 'mern-estate.appspot.com',
-//   messagingSenderId: '1078482850952',
-//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBihZw4vtN1rpdEKPzD-3MlyA3ZKq3-yL8",
   authDomain: "key2homes.firebaseapp.com",
@@ -37,3 +15,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Export the initialized Firebase app and analytics instance
+export { app, analytics };
